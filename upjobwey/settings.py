0d4c0d7ed2b676c480551f51ce628d5cdcb2285f
Daniel Milimo <dantemilimo@gmail.com>
2024-06-08 19:05:12 +0300

    last touches ck editor and tiny change

--- upjobwey/settings.py
+++ upjobwey/settings.py
@@ -244,25 +244,25 @@ CKEDITOR_CONFIGS = {
 }
 
 
-USE_S3 = os.getenv("USE_S3") == "TRUE"
+# USE_S3 = os.getenv("USE_S3") == "TRUE"
 
-if USE_S3:
-    # aws settings
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    # s3 static settings
-    # s3 static settings
-    STATIC_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "upjobwey.mediaStorage.StaticStorage"
-    # s3 public media settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "upjobwey.mediaStorage.PublicMediaStorage"
+# if USE_S3:
+#     # aws settings
+#     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+#     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+#     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+#     AWS_DEFAULT_ACL = "public-read"
+#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+#     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+#     # s3 static settings
+#     # s3 static settings
+#     STATIC_LOCATION = "static"
+#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+#     STATICFILES_STORAGE = "upjobwey.mediaStorage.StaticStorage"
+#     # s3 public media settings
+#     PUBLIC_MEDIA_LOCATION = "media"
+#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+#     DEFAULT_FILE_STORAGE = "upjobwey.mediaStorage.PublicMediaStorage"
 
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
